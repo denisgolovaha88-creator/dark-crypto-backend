ec09c6e71e746b18501e230f0fa3e151ad746424
denisgolovaha88-creator <denisgolovaha88@gmail.com>
2026-05-20 18:09:54 +0300

    Update setWebhook.js

--- api/setWebhook.js
+++ api/setWebhook.js
@@ -1,14 +1,6 @@
 module.exports = async (req, res) => {
-  const token = process.env.BOT_TOKEN;
-
-  const webhookUrl =
-    "https://dark-crypto-backend.vercel.app/api/telegram";
-
-  const response = await fetch(
-    `https://api.telegram.org/bot${token}/setWebhook?url=${webhookUrl}`
-  );
-
-  const data = await response.json();
-
-  res.status(200).json(data);
+  return res.status(200).json({
+    ok: true,
+    message: "Webhook route works"
+  });
 };
